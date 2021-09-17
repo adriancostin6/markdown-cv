5e1dfd493897cb378018cde9cc49cca63fe459ad
Adrian Costin <adriancostin6@gmail.com>
2021-09-17 20:31:39 +0300

    Add RO cv

--- deploy-cv.sh
+++ deploy-cv.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-jekyll build && cp _site/index.html ~/src/adriancostin6.github.io/cv \
-    && cp --recursive _site/media ~/src/adriancostin6.github.io/cv
+# Takes file path as command line argument, builds and deploys CV to it
+
+if [ "$#" -lt 1 ]; then
+    echo "Script that deploys CV"
+    echo "Usage: ./deploy-cv.sh PATH"
+    echo "Example: ./merge-pdf.sh ~/cv"
+    exit
+fi
+
+jekyll build && cp _site/index.html $1 \
+    && cp --recursive _site/media $1
